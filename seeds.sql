31b737e5cfdd955059e21ef886dc3591fa91d4b6
Armando Herrera Canelon <armando.herrera91@gmail.com>
2020-02-29 18:49:03 -0500

    Made changes to server,js, added schema and seeds files

---
+++ seeds.sql
@@ -0,0 +1,17 @@
+
+USE employeeDB;
+
+INSERT INTO department (name)
+VALUES ("Human Resources");
+
+INSERT INTO department (name)
+VALUES ("Program Management Office");
+
+INSERT INTO department (name)
+VALUES ("Operations");
+
+INSERT INTO department (name)
+VALUES ("Accounts Security");
+
+INSERT INTO department (name)
+VALUES ("Information Technology Office");
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: "localhost",
 
   // Your port; if not 3306
-  port: 3308,
+  port: 3306,
 
   // Your username
   user: "root",
@@ -18,5 +18,132 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connection was succesfull dude!");
-    readColleges();
+      promptMainMenu();
   });
+
+promptMainMenu = () => {
+
+  inquirer
+    .prompt({
+      name: "mainMenuSelection",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "Add department",
+        "Add role",
+        "Add employee",
+        "View departments",
+        "View roles",
+        "View employees",
+        "Update employee roles",
+        "Quit"
+      ]
+    })
+    .then(response => {
+       switch (response.mainMenuSelection) {
+         case "Quit": 
+          connection.end();
+          break;
+        case "Add department":
+          promptForNewDepartment();
+          break;
+        case "View departments":
+          viewAllDepartments();
+          break;
+        case "Add employee":
+          promptToAddEmployee();
+          break;
+        default:
+          connection.end();
+       }
+    }) 
+}
+
+promptForNewDepartment = () => {
+  inquirer
+    .prompt({
+      name: "newDepartment",
+      type: "input",
+      message: "What department would you like to add?"
+    })
+    .then(response => {
+      // Add to the database
+      let queryString = `INSERT INTO employeeDB.department (name) VALUES ("${response.newDepartment}")`;
+                              //values from our object below are substituted into "?" above.
+      let query = connection.query(queryString, (err, res) => {
+
+        if (err) {
+          console.log(err);
+        }
+        promptMainMenu();
+      })
+    })
+}
+
+viewAllDepartments = () => {
+  let queryString = `SELECT * FROM employeeDB.department`;
+                              //values from our object below are substituted into "?" above.
+  let query = connection.query(queryString, (err, res) => {
+
+    if (err) {
+      console.log(err);
+    }
+    else {
+      console.table(res)
+    }
+    promptMainMenu();
+  });
+    
+}
+
+promptToAddEmployee = () => {
+  /*
+  //Query the DB for a list of roles
+  //roleQuery = "SELECT title FROM...."
+  //let  connection1 = connection.query(roleQuery, (err, roles) => {
+    if (err) {
+
+    } else {
+      managerQuery = "SELECT lastName FROM...."
+      let connecton2 = connection.query(managerQuery, (err, managers) => {
+        if (err) {
+
+        } else {
+          inquirer
+          .prompt({
+            name: "employeeFirstName",
+            type: "input",
+            message: "What is the employee's first name?"
+          },
+          {
+            name: "employeeLastName",
+            type: "input",
+            message: "What is the employee's last name?"
+          },
+          {
+            name: "employeeRole",
+            type: "list",
+            message: "What is the employee's role?",
+            choices: []
+          },
+          {
+            name: "employeeManager",
+            type: "list",
+            message: "Who is the employee's manager?",
+            choices: []
+          })
+          .then(response => {
+
+
+          });
+        }
+      })
+    }
+  })
+    //After that completes Query the DB for a list of managers
+      //After that completes prompt the user for all the questions about the new employee
+
+      */
+  
+
+}
